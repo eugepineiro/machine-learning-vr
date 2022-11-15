f19305476a6f804d972b7e101f9f555dbdb8be1c
eugepineiro <epineiro@itba.edu.ar>
2022-11-15 16:27:09 -0300

    Add stop multiple audios

--- Assets/Scripts/RaycastController.cs
+++ Assets/Scripts/RaycastController.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class RaycastController : MonoBehaviour
 {
-    private AudioSource _activeAudioSource;
+    private List<AudioSource> _activeAudioSources;
 	private const float RAYCAST_MAX_DISTANCE = 50.0F; 
 
-    void Start()
-    {
-  
-        
-    }
+	void Start() 
+	{ 
+		_activeAudioSources = new List<AudioSource>();
+	}
 
     void Update()
     {
         RaycastHit[] hits;
         hits = Physics.RaycastAll(this.transform.position, this.transform.forward, RAYCAST_MAX_DISTANCE);
-        //var didHit = Physics.Raycast(this.transform.position, this.transform.forward, out var hit, RAYCAST_MAX_DISTANCE);
+      
 		if(hits.Length > 0) {
 			for (int i = 0; i < hits.Length; i++)
         	{
@@ -30,14 +29,18 @@ public class RaycastController : MonoBehaviour
             		GameObject audio = GameObject.Find($"Audio{hit.transform.gameObject.name}");
            
             		AudioSource _audioSource = audio.GetComponent<AudioSource>();
-            		if (_audioSource != _activeAudioSource) _activeAudioSource = _audioSource;
+            		if (!_activeAudioSources.Contains(_audioSource)) _activeAudioSources.Add(_audioSource);
             
-            		if (!_activeAudioSource.isPlaying) _activeAudioSource.Play();
+            		if (!_audioSource.isPlaying) _audioSource.Play();
         		}
         	
 			}
 		} else {
- 			if (_activeAudioSource != null) _activeAudioSource.Stop();
+			foreach (AudioSource audio in _activeAudioSources) { 
+				audio.Stop();
+			} 
+			_activeAudioSources.Clear();
+ 
 		}
  
         Debug.DrawRay(this.transform.position, this.transform.forward * RAYCAST_MAX_DISTANCE, Color.green,15);
